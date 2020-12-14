8b19a97ec37a393f58c28fbbbe32f363e3b6a3bd
a.shander <a.shander@otgroup.kz>
2020-12-14 17:18:35 +0600

    Add Docker file for web app

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export FLASK_APP=ident_cdr
-export IDENT_INTEGRATION_KEY=1234567
-export MYSQL_USER=root
-export MYSQL_PASSWORD=3nf82s
-export AMI_USER=userevents
-export AMI_PASSWORD=password
-flask run
